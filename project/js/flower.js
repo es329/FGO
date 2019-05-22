f4c319e6f54f4e47d5c87f6107d9f10291b8658d
es329 <yangzhaocheng123@qq.com>
2019-05-22 22:21:09 +0800

    完善flower.js

--- project/js/flower.js
+++ project/js/flower.js
@@ -1,6 +1,6 @@
 $(function(){
   window.onscroll=function(){
-    var top=$(document).scrollTop();
+    var top=$(document).scrollTop()+210;
     // if(top<0.1182){top=0.1182}
     // else if(top>1.6718){top=1.6718}
     $(".main-container .flower").css("margin-top",`${top}px`);
