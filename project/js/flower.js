d6e22286852740a3b1bf05c97f1e69a330af5ac1
es329 <yangzhaocheng123@qq.com>
2019-05-23 16:07:55 +0800

    finish gallery.html
    完成了gallert分页

--- project/js/flower.js
+++ project/js/flower.js
@@ -1,10 +1,10 @@
 $(function(){
+  //添加一个时间函数，当窗口滚动的时候使.flower的margin-top随着改变，使.flower保持在窗口的固定位置
   window.onscroll=function(){
-    var top=$(document).scrollTop()+210;
+    var top=$(document).scrollTop()+200;
     // if(top<0.1182){top=0.1182}
     // else if(top>1.6718){top=1.6718}
     $(".main-container .flower").css("margin-top",`${top}px`);
     //$(".main-container .flower").css("margin-top",`${top*100}%`);
   }
 })
-
