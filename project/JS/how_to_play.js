33f5b9d97a55a9717004eec30af21dfc90cb0e57
es329 <yangzhaocheng123@qq.com>
2019-05-25 17:58:49 +0800

    完成了how_to_play.js第一版

--- project/JS/how_to_play.js
+++ project/JS/how_to_play.js
@@ -1,3 +1,15 @@
 $(function(){
-    
+    $(".layout-left>ul").on("click","li",function(){
+		$this=$(this);
+		//console.log($this.children().attr("src").split(".")[1].slice(-1));
+		//当前点击的li增加class.cur，其他的li去掉class.cur
+		$this.addClass("cur").siblings("li").removeClass("cur");
+		//通过图片的src，得到点击是第几个li
+		let n=$this.children().attr("src").split(".")[1].slice(-1);
+		//同时找到.layout-right对应的li
+		let $tar=$(`.layout-right>ul>li:nth-child(${n})`);
+		//console.log($tar);
+		//.layout-right中对应的li增加class.cur，其他的li去掉class.cur
+		$tar.addClass("cur").siblings("li").removeClass("cur");
+    })
 })
